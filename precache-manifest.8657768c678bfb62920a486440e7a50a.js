e2568dc83273562cf8754e18dc53c396df4d263d
bala876 <milky016918@gmail.com>
2021-06-01 15:26:31 +0530

    Updates

---
+++ precache-manifest.8657768c678bfb62920a486440e7a50a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b01b9675a35c54dadb4",
+    "url": "/Cart/static/css/main.4e7bb1e3.chunk.css"
+  },
+  {
+    "revision": "8b01b9675a35c54dadb4",
+    "url": "/Cart/static/js/main.8b01b967.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/Cart/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "a4e28177df1ddb514d66",
+    "url": "/Cart/static/js/runtime~main.a4e28177.js"
+  },
+  {
+    "revision": "da9915c1fa6e81461279513da2a5c81d",
+    "url": "/Cart/static/media/rose.da9915c1.jpg"
+  },
+  {
+    "revision": "08d6e9df67869111292153fb424cc7dd",
+    "url": "/Cart/static/media/cookie.08d6e9df.jpg"
+  },
+  {
+    "revision": "eb21a80f70f775ceeea0507d073eb79c",
+    "url": "/Cart/static/media/bg.eb21a80f.jpg"
+  },
+  {
+    "revision": "681def73ed005a281bb8b0b2d9fa8c02",
+    "url": "/Cart/index.html"
+  }
+];
